ae7e5553d2824813e5b1440e00c94994bc0f45f0
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-11-29 21:25:28 +0200

    done

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -2,10 +2,9 @@ import { useContext1 } from '../contexts/Context1';
 
 export function Component1() {
     const { data, setData } = useContext1();
-    console.log(1)
     return (
         <div>
-            name: {data.name} surname: {data.surname}
+            name: {data.name} <br/>surname: {data.surname}
         </div>
     );
 }
--- src/components/Component2.jsx
+++ src/components/Component2.jsx
@@ -2,10 +2,9 @@ import { useContext2 } from '../contexts/Context2';
 
 export function Component2() {
     const { data, setData } = useContext2();
-    console.log(2)
     return (
         <div>
-            name123: {data.name} surname: {data.surname}
+            name: {data.name} <br/>surname: {data.surname}
         </div>
     );
 }
